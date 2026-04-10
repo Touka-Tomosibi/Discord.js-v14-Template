94e0ac4cb062ee77daf720edf08cf30ee40f62a3
Touka-Tomosibi <160579895+Touka-Tomosibi@users.noreply.github.com>
2026-04-10 11:41:40 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -16,7 +16,7 @@ for (const folder of commandFolders) {
 	
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
@@ -28,15 +28,15 @@ for (const folder of commandFolders) {
 	}
 }
 
-// Construct and prepare an instance of the REST module
+
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
+
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
@@ -44,7 +44,7 @@ const rest = new REST().setToken(token);
 
 		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
+		
 		console.error(error);
 	}
 })();
