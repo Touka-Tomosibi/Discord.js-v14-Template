135ed7c7f5ff78c02925f173b4f88e5a3e2b35f0
Touka-Tomosibi <160579895+Touka-Tomosibi@users.noreply.github.com>
2026-05-12 13:06:23 +0900

    Create  code.js

---
+++ events/code.js
@@ -0,0 +1,14 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.MessageCreate,
+	async execute(message) {
+        if (message.author.bot) return;
+        
+        if (message.content.startsWith('テンプレートを教えて')) { 
+            
+            await message.channel.send('');//送信するメッセージ
+            
+         }
+	},
+};
