7c793ddef1a587fed60f3cd250ddb9fb06a31b7b
Touka-Tomosibi <160579895+Touka-Tomosibi@users.noreply.github.com>
2026-05-12 13:08:39 +0900

    Create Goodmorning.js

---
+++ events/Goodmorning.js
@@ -0,0 +1,14 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.MessageCreate,
+	async execute(message) {
+        if (message.author.bot) return;
+        
+        if (message.content.startsWith('おはよ')) { 
+
+            await message.channel.send('おやすみ');
+            
+         }
+	},
+};
